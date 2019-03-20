1499b355b97aaf47b86c3a72471cd1fb86712d80
Andrew.Beletskiy <andrew.beletskiy@adraba.io>
2019-03-20 10:53:46 +0200

    Decreased width of lines

--- src/PlayingGame.js
+++ src/PlayingGame.js
@@ -23,55 +23,59 @@ function getTable({ blocks, players, nextMove }) {
   const tableHeight = height * 2 + 1;
   const getType = (row, col) => {
     if (row % 2 === 0 && col % 2 === 0) {
-      return 'space'
+      return "space";
     }
     if (row % 2 === 0 && col % 2 === 1) {
-      return 'horizontal'
+      return "horizontal";
     }
     if (row % 2 === 1 && col % 2 === 0) {
-      return 'vertical'
+      return "vertical";
     }
-    return 'block'
-  }
-
+    return "block";
+  };
 
   const getCell = (row, col) => {
-    const type = getType(row, col)
-    const cell = { type, row, col }
-    const blockRow = Math.floor(row / 2)
-    const blockCol = Math.floor(col / 2)
-    if (type === 'space') {
-      return cell
+    const type = getType(row, col);
+    const cell = { type, row, col };
+    const blockRow = Math.floor(row / 2);
+    const blockCol = Math.floor(col / 2);
+    if (type === "space") {
+      return cell;
     }
-    if (nextMove && type === nextMove.type && nextMove.row === blockRow && nextMove.col === blockCol) {
-      cell.isNext = true
+    if (
+      nextMove &&
+      type === nextMove.type &&
+      nextMove.row === blockRow &&
+      nextMove.col === blockCol
+    ) {
+      cell.isNext = true;
     }
-    if (type === 'horizontal') {
+    if (type === "horizontal") {
       if (blockRow === blocks.length) {
-        const block = blocks[blockRow-1][blockCol]
-        cell.playerId = block.bottom
+        const block = blocks[blockRow - 1][blockCol];
+        cell.playerId = block.bottom;
       } else {
-        const block = blocks[blockRow][blockCol]
-        cell.playerId = block.top
+        const block = blocks[blockRow][blockCol];
+        cell.playerId = block.top;
       }
     }
-    if (type === 'vertical') {
+    if (type === "vertical") {
       if (blockCol === blocks[0].length) {
-        const block = blocks[blockRow][blockCol-1]
-        cell.playerId = block.right
+        const block = blocks[blockRow][blockCol - 1];
+        cell.playerId = block.right;
       } else {
-        const block = blocks[blockRow][blockCol]
-        cell.playerId = block.left
+        const block = blocks[blockRow][blockCol];
+        cell.playerId = block.left;
       }
     }
-    if (type === 'block') {
-      const block = blocks[blockRow][blockCol]
-      cell.playerId = block.occupiedBy 
+    if (type === "block") {
+      const block = blocks[blockRow][blockCol];
+      cell.playerId = block.occupiedBy;
     }
     if (cell.playerId) {
-      cell.color = players.find(p => p.id === cell.playerId).color
+      cell.color = players.find(p => p.id === cell.playerId).color;
     }
-    return cell
+    return cell;
   };
   return Array.from({ length: tableHeight }, (row, rowInd) =>
     Array.from({ length: tableWidth }, (cell, colInd) =>
@@ -80,75 +84,121 @@ function getTable({ blocks, players, nextMove }) {
   );
 }
 
-function Board({ horizontal, vertical, filledBlocks, players, onMove, currentPlayerId }) {
+function Board({
+  horizontal,
+  vertical,
+  filledBlocks,
+  players,
+  onMove,
+  currentPlayerId
+}) {
   const [nextMove, setNextMove] = useMergeState(null);
   const blocks = getBlocks({ horizontal, vertical, filledBlocks, players });
   const table = getTable({ blocks, nextMove, players });
-  const currentPlayer = players.find(p => p.id === currentPlayerId)
-  const renderCell = cell => {
-    if (cell.type === 'space') {
-      return <td className="cell space" key={`${cell.row}-${cell.col}`}/>
+  const currentPlayer = players.find(p => p.id === currentPlayerId);
+
+  const handleMove = nextMove => {
+    if (!nextMove) return;
+    if (nextMove.type === "horizontal") {
+      if (horizontal[nextMove.row][nextMove.col]) return setNextMove(null);
     }
-    if (cell.type === 'horizontal') {
-      const handleHorizontal = () => {
-        setNextMove({
-          type: 'horizontal',
-          row: Math.floor(cell.row / 2),
-          col: Math.floor(cell.col / 2),
-          playerId: currentPlayerId,
-        })
-      }
-      const classes = ['cell', 'horizontal', nextMove && cell.isNext ? 'next-move' : 'not-next-move'].join(' ')
-      const style = cell.color
-        ? { backgroundColor: cell.color }
-        : (
-          cell.isNext
-            ? { backgroundColor: currentPlayer.color }
-            : {}
-        )
-      return <td onMouseEnter={handleHorizontal} className={classes} style={style} key={`${cell.row}-${cell.col}`}/>
+    if (nextMove.type === "vertical") {
+      if (vertical[nextMove.row][nextMove.col]) return setNextMove(null);
     }
-    if (cell.type === 'vertical') {
-      const handleVertical = () => {
-        setNextMove({
-          type: 'vertical',
-          row: Math.floor(cell.row / 2),
-          col: Math.floor(cell.col / 2),
-          playerId: currentPlayerId
-        })
-      }
-      const style = cell.color
-        ? { backgroundColor: cell.color }
-        : (
-          cell.isNext
-            ? { backgroundColor: currentPlayer.color }
-            : {}
-        )
-      const classes = ['cell', 'vertical', cell.isNext ? 'next-move' : 'not-next-move'].join(' ')
-      return <td onMouseEnter={handleVertical} className={classes} style={style} key={`${cell.row}-${cell.col}`}/>
-    }
-    const { color } = cell
-    return <td className="cell block" style={color ? {backgroundColor: color } : {}} key={`${cell.row}-${cell.col}`}/>;
+    onMove(nextMove);
+    setNextMove(null);
   };
 
-  const handleMove = () => {
-    if (!nextMove) return
-    if (nextMove.type === 'horizontal') {
-      if (horizontal[nextMove.row][nextMove.col]) return setNextMove(null)
+  const renderCell = cell => {
+    if (cell.type === "space") {
+      return <td className="cell space" key={`${cell.row}-${cell.col}`} />;
     }
-    if (nextMove.type === 'vertical') {
-      if (vertical[nextMove.row][nextMove.col]) return setNextMove(null)
+    if (cell.type === "horizontal") {
+      const getPossibleMove = () => ({
+        type: "horizontal",
+        row: Math.floor(cell.row / 2),
+        col: Math.floor(cell.col / 2),
+        playerId: currentPlayerId
+      });
+      const handleHorizontalMouseEnter = () => {
+        console.log('hey')
+        setNextMove(getPossibleMove());
+      };
+      const handleHorizontalClick = () => handleMove(getPossibleMove());
+      const classes = [
+        "cell",
+        "horizontal",
+        nextMove && cell.isNext ? "next-move" : "not-next-move",
+        cell.playerId ? "occupied" : "not-occupied"
+      ].join(" ");
+      const style = cell.color
+        ? { backgroundColor: cell.color }
+        : cell.isNext
+        ? { backgroundColor: currentPlayer.color }
+        : {};
+      return (
+        <td className={classes} style={style} key={`${cell.row}-${cell.col}`}>
+          <div
+            className="mouse-target horizontal"
+            onMouseMove={handleHorizontalMouseEnter}
+            onClick={handleHorizontalClick}
+          />
+        </td>
+      );
     }
-    onMove(nextMove)
-    setNextMove(null)
-  }
+    if (cell.type === "vertical") {
+      const getPossibleMove = () => ({
+        type: "vertical",
+        row: Math.floor(cell.row / 2),
+        col: Math.floor(cell.col / 2),
+        playerId: currentPlayerId
+      });
+      const handleVerticalMouseEnter = () => {
+        setNextMove(getPossibleMove());
+      };
+      const handleVerticalClick = () => {
+        handleMove(getPossibleMove());
+      };
+      const style = cell.color
+        ? { backgroundColor: cell.color }
+        : cell.isNext
+        ? { backgroundColor: currentPlayer.color }
+        : {};
+      const classes = [
+        "cell",
+        "vertical",
+        cell.isNext ? "next-move" : "not-next-move",
+        cell.playerId ? "occupied" : "not-occupied"
+      ].join(" ");
+      return (
+        <td className={classes} style={style} key={`${cell.row}-${cell.col}`}>
+          <div
+            className="mouse-target vertical"
+            onMouseEnter={handleVerticalMouseEnter}
+            onClick={handleVerticalClick}
+          />
+        </td>
+      );
+    }
+    const { color } = cell;
+    return (
+      <td
+        className="cell block"
+        style={color ? { backgroundColor: color } : {}}
+        key={`${cell.row}-${cell.col}`}
+      />
+    );
+  };
 
   return (
     <div className="board">
-      <table className="board-table" onClick={handleMove}>
+      <table className="board-table" onClick={() => handleMove(nextMove)}>
         <tbody>
           {table.map((row, rowInd) => (
-            <tr className={rowInd % 2 === 0 ? "horizontals-row" : "blocks-row"} key={rowInd}>
+            <tr
+              className={rowInd % 2 === 0 ? "horizontals-row" : "blocks-row"}
+              key={rowInd}
+            >
               {row.map(renderCell)}
             </tr>
           ))}
@@ -170,7 +220,7 @@ export default function PlayingGame({ game, onMove }) {
   return (
     <div className="playing-game-container">
       <GameStatus {...game} />
-      <Board {...game} onMove={onMove}/>
+      <Board {...game} onMove={onMove} />
       <MoveHistory moves={game.moveHistory} />
     </div>
   );
